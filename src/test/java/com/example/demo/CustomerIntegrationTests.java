5e7cb236ffcf8cd146d9dd48563b812b7dc313f1
atasios <aris.tasios@vodafone.com>
2023-03-24 09:42:55 -0400

    extra commands

--- src/test/java/com/example/demo/CustomerIntegrationTests.java
+++ src/test/java/com/example/demo/CustomerIntegrationTests.java
@@ -5,10 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.output.OutputFrame;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.io.IOException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -35,7 +38,11 @@ class CustomerIntegrationTests {
 	}
 
 	@Test
-	void when_using_a_clean_db_this_should_be_empty() {
+	void when_using_a_clean_db_this_should_be_empty() throws IOException, InterruptedException {
+		container.withClasspathResourceMapping("application.properties", "/tp/application.properties", BindMode.READ_ONLY);
+		container.execInContainer("la", "-la");
+		Integer containerHostPort = container.getMappedPort(3306);
+		String stdout = container.getLogs(OutputFrame.OutputType.STDOUT);
 		List<Customer> customers = customerDao.findAll();
 		assertThat(customers).hasSize(2);
 	}
